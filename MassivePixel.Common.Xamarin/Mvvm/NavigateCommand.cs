78bb42a150acaab948ce79c60f62d835c2fd8ad9
Toni Petrina <tonipetrina@hotmail.com>
2015-01-19 16:31:36 +0100

    Initial project added; NavigationCommand added

---
+++ MassivePixel.Common.Xamarin/Mvvm/NavigateCommand.cs
@@ -0,0 +1,20 @@
+﻿using GalaSoft.MvvmLight.Ioc;
+using Xamarin.Forms;
+
+namespace MassivePixel.Common.Xamarin.Mvvm
+{
+    public class NavigateCommand<TPage> : Command
+        where TPage : Page, new()
+    {
+        public NavigateCommand()
+            : base(Navigate)
+        {
+        }
+
+        private static void Navigate()
+        {
+            var ns = SimpleIoc.Default.GetInstance<INavigation>();
+            ns.PushAsync(new TPage());
+        }
+    }
+}
